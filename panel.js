7cc73e0bb57f31574fb11a44583b63f426281e43
trom6one <trom6one@gmail.com>
2020-06-06 10:21:53 +1100

    1

--- panel.js
+++ panel.js
@@ -2,12 +2,17 @@ var token = "";
 var tuid = "";
 var ebs = "";
 
+const helpUrlEng = "https://docs.google.com/document/d/e/2PACX-1vQxQnaIyGGyFOVftRG6Mfoj85xxV8N3r_4pkpBYeMTnx8YJKU1ZscnKduDM5lE40ULuk3FckTbTc5ft/pub"
+const helpUrlRus = "https://docs.google.com/document/d/e/2PACX-1vQr3kGqHZKu7YCSZ40TybkN1rhRec6xKMYwaP0XXKNqDvyGI4RFLpOYozJzXaASzYDAG0i13UYBGoTz/pub"
+
+
 // because who wants to type this every time?
 var twitch = window.Twitch.ext;
 
 // create the request options for our Twitch API calls
 var requests = {
-    set: postRequest('POST', 'amount'),
+    setPlus: postRequest('POST', 'amount'),
+    setMinus: postRequest('POST', 'demount'),
     get: getRequest('GET', 'query')
 };
 
@@ -44,7 +49,7 @@ twitch.onAuthorized(function(auth) {
     tuid = auth.userId;
 
     // enable the button
-    $('#toxic').removeAttr('disabled');
+    $('#button-toxic').removeAttr('disabled');
 
     setAuth(token);
     
@@ -54,8 +59,13 @@ twitch.onAuthorized(function(auth) {
 ///////////////////////////
 function localUpdateLine(amount) {
     $(".progress .water").css("top", 100 - amount + "%");
+    // $(".progress .water").animate({ top: 100 - amount + "%" }, 'easeInOutCubic', function(){ 
+    //     /* animation comlete */ 
+    // });
 
-    percentUpdate(top);
+    // % text
+    var top = $("#water")[0].style.top;
+    $('#percent').text((100 - parseFloat(top)).toFixed(1) + '%');
 }
 
 function listenBroadcast(){
@@ -81,22 +91,43 @@ function uiFeedback() {
     }
 }
 
-function percentUpdate(){
-    var top = $("#water")[0].style.top;
-    $('#percent').text((100 - parseFloat(top).toFixed(2)) + '%');
-}
 
 ///
 
 $(function() {    
-    $('#toxic').click(function() {
-        console.log("click")
+    // $('#panel-right').click(function() {
+    //     if($('#panel-right').hasClass("panel-right-closed")){
+    //         $('#panel-right').removeClass('panel-right-closed');
+    //     }
+    //     else{
+    //         $('#panel-right').addClass('panel-right-closed');
+    //     }
+    // });   
+
+    $("#button-help").click(function() {
+        var language = twitch.onContext(function(context) {return context.language });
+        console.log(`language = ${language}`);
+        var url = language == "ru" ? helpUrlRus : helpUrlEng;
+        console.log(`url = ${url}`);
+        window.open(url, "_blank");
+    });
+
+    $('#button-toxic').click(function() {
+        // console.log("click")
         if(!token) {return console.log('Not autorized!');}
 
         uiFeedback();
 
-        $.ajax(requests.set);
+        $.ajax(requests.setPlus);
     });
+
+    $('#button-untoxic').click(function() {
+        // console.log("click")
+        if(!token) {return console.log('Not autorized!');}
+
+        $.ajax(requests.setMinus);
+    });
+
     listenBroadcast();
 });
 
